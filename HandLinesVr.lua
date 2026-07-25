f18a1fd2179dd00549c1f0a871492aed593dee0b
Luay <luayrbx@gmail.com>
2026-07-25 20:54:29 -0300

    Update HandLinesVr.lua to version 1.9

--- HandLinesVr.lua
+++ HandLinesVr.lua
@@ -1,5 +1,5 @@
 -- made by haker999
--- V1.8
+-- V1.9
 
 local Players = game:GetService("Players")
 
@@ -27,16 +27,16 @@ end
 local function createLine(hand)
 	local attachment0 = Instance.new("Attachment")
 	attachment0.Name = "HandLineStart"
-	attachment0.Position = Vector3.new(0, 0, -0.15)
+	attachment0.Position = Vector3.new(0, -0.15, 0)
 	attachment0.Parent = hand
 
 	local attachment1 = Instance.new("Attachment")
 	attachment1.Name = "HandLineEnd"
-	attachment1.Position = Vector3.new(0, 0, -LINE_LENGTH)
+	attachment1.Position = Vector3.new(0, -LINE_LENGTH, 0)
 	attachment1.Parent = hand
 
 	local beam = Instance.new("Beam")
-	beam.Name = "HandForwardLine"
+	beam.Name = "HandDownLine"
 	beam.Attachment0 = attachment0
 	beam.Attachment1 = attachment1
 	beam.Width0 = START_WIDTH
